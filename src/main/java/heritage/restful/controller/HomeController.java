9f995f8e88b69dc4b1d5f96c946dbbcf7274faa7
Patrick Elder <patelder95@gmail.com>
2020-03-10 13:33:38 -0400

    Database Connection and port is 8081

---
+++ src/main/java/heritage/restful/controller/HomeController.java
@@ -0,0 +1,25 @@
+package heritage.restful.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @Controller
+    public class HomeController {
+
+        // inject via application.properties
+        @Value("${welcome.message:test}")
+        private String message = "Hello World";
+
+        @RequestMapping("/")
+        public String welcome(Map<String, Object> model) {
+            model.put("message", this.message);
+            return "welcome";
+        }
+
+    }
+
--- src/test/java/com/example/restservice/ApplicationTests.java
+++ src/test/java/com/example/restservice/ApplicationTests.java
@@ -1,15 +1,7 @@
 package com.example.restservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SummerApplicationTests {
 
     @Test
